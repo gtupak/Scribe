53e00adb0e47270804ddcb74eccbf54bdc696072
Gabriel T <gabriel.tapuc@gmail.com>
2017-09-27 13:29:54 -0400

    Final app implementation

---
+++ Scribe/View/CircleButton.swift
@@ -0,0 +1,28 @@
+//
+//  CircleButton.swift
+//  Scribe
+//
+//  Created by Gabriel T on 2017-09-27.
+//  Copyright © 2017 Gabriel T. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 30.0 {
+        didSet {
+            setupView()
+        }
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        setupView()
+    }
+    
+    func setupView() {
+        layer.cornerRadius = cornerRadius
+    }
+
+}
